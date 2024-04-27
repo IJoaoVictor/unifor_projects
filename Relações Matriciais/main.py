5cbb4e94be3b19193c9e759ed2067f34afcee692
João Victor <89043251+IJoaoVictor@users.noreply.github.com>
2024-04-27 18:09:05 -0300

    Adicionando função assimétrica, transitiva, relação de ordem, equivalência, minimais e maximais
    Falta fecho simétrico

--- Relações Matriciais/main.py
+++ Relações Matriciais/main.py
@@ -41,6 +41,7 @@ def construir_matriz_adjacencia(estacoes, linhas_por_estacao):
     return matriz_adjacencia
 
 # Função para exibir a matriz 
+
 def printMatrix(matriz):
   
   # len(matriz) = número de linhas
@@ -97,7 +98,7 @@ def multiMatrix(matrizA, matrizB):
   else:
     print("\nOrdens de matrizes não casam para produto matricial")
 
-#Itens Restantes
+#Itens
 
 # - Verificar se a matriz é reflexiva
 
@@ -107,10 +108,10 @@ def isReflexive(matrix):
   
   for i in range(len(matrix)):
       if matrix[i][i] != 1:
-        print("\nA matriz não é reflexiva")
+        print("A matriz não é reflexiva")
         return False
 
-  print("\nA matriz é reflexiva")
+  print("A matriz é reflexiva")
   return True
   
 # - Verificar se a matriz é simétrica
@@ -121,10 +122,10 @@ def isSymmetric(matrix):
   for i in range(len(matrix)):
       for j in range(len(matrix[0])):
           if matrix[i][j] != matrix[j][i]:
-            print("\nA matriz não é simétrica")
+            print("A matriz não é simétrica")
             return False
 
-  print("\nA matriz é simétrica")
+  print("A matriz é simétrica")
   return True
 
 # - Verificar se a matriz é assimétrica
@@ -149,11 +150,124 @@ def isAssymmetric(matrix):
   print('A matriz é assimétrica')
   return True
              
-# - Verificar se a matriz é antissimétrica 
+# - Verificar se a matriz é antissimétrica
+def isAntisymmetric(matriz):
+  
+  # - Verificar se a matriz é antissimétrica
+  for i in range(len(matriz)):
+    for j in range(len(matriz[0])):
+      if matriz[i][j] == 1 and matriz[j][i] == 1 and i != j:
+        print('A matriz não é antissimétrica')
+        return False
+
+  print('A matriz é antissimétrica')
+  return True
+
 # - Verificar se a matriz é transitiva
-# - Identificar se a Relação é de Ordem, se é de Equivalência e, caso existam, quem são os elementos maximais e minimais, e o maior e menor elemento da relação.
-# - Identificar e completar fecho
+def isTransitive(matriz):
+  # - Verificar se a matriz é transitiva
+  for i in range(len(matriz)):
+    for j in range(len(matriz[0])):
+      if matriz[i][j] == 1:
+        for k in range(len(matriz[0])):
+          if matriz[j][k] == 1 and matriz[i][k] !=1:
+            print('A matriz não é transitiva')
+            return False
+  
+  print('A matriz é transitiva')
+  return True
+  
+# - Ientificar se a Relação é de Ordem, se é de Equivalência e, caso existam, quem são os elementos maximais e minimais, e o maior e menor elemento da relação.
+
+def isOrderRelation(matrix):
+
+  if isReflexive(matrix) and isAntisymmetric(matrix) and isTransitive(matrix):
+    print('A matriz representa uma relação de ordem')
+    return True
+  else:
+    print('A matriz não representa uma relação de ordem')
+    return False
+
+def isEquivalenceRelation(matrix):
+  if isReflexive(matrix) and isSymmetric(matrix) and isTransitive(matrix):
+    print('A matriz representa uma relação de equivalência')
+    return True
+  else:
+    print('A matriz não representa uma relação de equivalência')
+    return False
+    
+def maximals_minimals(matrix):
+
+  maximais = []
+  minimais = []
+  
+  for i in range(len(matrix)):
+    
+      isMaximal = True
+      isMinimal = True
+  
+      for j in range(len(matrix[0])):
+          if matrix[i][j] == 1 and i != j:  # Verifica se há uma relação de i para j
+              isMaximal = False
+          if matrix[j][i] == 1 and i != j:  # Verifica se há uma relação de j para i
+              isMinimal = False
+  
+      if isMaximal:
+          maximais.append(i+1)
+      if isMinimal:
+          minimais.append(i+1)
+
+  if maximais != []:
+    print("Elementos maximais:", maximais)
+  else:
+    print("Não há elementos maximais")
+
+  if minimais != []: #Acusa erro mesmo
+    print("Elementos minimais:", minimais) 
+  else:
+    print("Não há elementos minimais")
+
+def findLargestElement(matrix):
+  
+  linePosition = 0
+  # Verifica a linha que possui apenas 1
+  for row in matrix:
+      isMaxLine = True
+      for element in row:
+          if element != 1:
+              isMaxLine = False
+              break
+      if isMaxLine:
+          return linePosition
+      linePosition += 1
+    
+  return None  # Retorna None se não encontrar nenhuma linha de 1s
+
+def findSmallestElement(matrix):
+
+  #Analisa a linha que possui apenas 1 da transposta, que é a coluna da original
+  transposedMatrix = transposeMatrix(matrix)
+  return findLargestElement(transposedMatrix)
+
+#Chama as duas funções anteriores
+
+def callElementsFinder(matrix):
+  
+  if findLargestElement(matrix) is not None:
+    print("O maior elemento é:", findLargestElement(matrix) + 1) 
+  else:
+    print("Não há maior elemento")
+
+  if findSmallestElement(matrix) is not None: #Acusa erro mesmo
+    print("O menor elemento é:", findSmallestElement(matrix) + 1)
+  else:
+    print("Não há menor elemento")
+
+
+# Fecho Simétrico
+
 adjacencyMatrix = construir_matriz_adjacencia(estacoes, linhas_por_estacao)
+printMatrix(adjacencyMatrix)
 
 busMatrix = [
   [0,1,0,0,0,1],
@@ -163,6 +277,12 @@ busMatrix = [
   [0,0,0,0,0,1],
   [1,0,0,1,1,0]
 ]
+
+# Composição das Matrizes
+
+print('\nMatriz Composta\n')
+printMatrix(multiMatrix(busMatrix, adjacencyMatrix))
+
 
 reflexiva = [
   [1,1,0],
@@ -188,18 +308,25 @@ antisimetrica = [
   [0,1,0]
 ]
 
-# Imprimir a matriz de adjacência
-printMatrix(adjacencyMatrix)
+transitiva = [
+  [0,1,0],
+  [0,0,0],
+  [0,1,0]
+]
 
-print("\nMatrix Composta\n")
-compositionMatrix = multiMatrix(busMatrix, adjacencyMatrix);
-printMatrix(compositionMatrix)
+minimals_maximals = [
+  [1,1,1],
+  [0,1,0], #maximal = 2 e 3 e minimal = 1
+  [0,0,1]  #maior = 1 e menor = não tem
+]
 
-print("\nMatrix Transposta\n")
-transposeMatrix = transposeMatrix(adjacencyMatrix);
-printMatrix(transposeMatrix)
+#isReflexive(reflexiva)
+#isSymmetric(simetrica)
+#isAssymmetric(assimetrica)
+#isAntisymmetric(antisimetrica)
+#isTransitive(transitiva)
+#isOrderRelation(minimals_maximals)
+#isEquivalenceRelation(minimals_maximals)
 
-
-isReflexive(reflexiva)
-isSymmetric(simetrica)
-isAssymmetric(assimetrica)
+maximals_minimals(minimals_maximals)
+callElementsFinder(minimals_maximals)
